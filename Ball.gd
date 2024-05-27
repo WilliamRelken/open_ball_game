e3c4221c5dffb8d6f7cbda079c3e3f7fa0ed5d96
William Relken <relkenwilliam@gmail.com>
2024-05-26 20:37:38 -0400

    added hitting ball in a direction

--- Ball.gd
+++ Ball.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-signal change_direction
+signal change_direction(dir: Vector2, mult: float)
 
+@export var rand_start = false
 var direction: Vector2
 var SPEED = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	direction = Vector2(1,1).rotated(deg_to_rad(randi_range(0,359))) * SPEED
+	if rand_start:
+		direction = Vector2(1,1).rotated(deg_to_rad(randi_range(0,359))) * SPEED
 
 func _process(delta):
 	var spd = SPEED
@@ -22,6 +24,9 @@ func _on_area_2d_body_entered(body: Node2D):
 		direction.y *= -1
 
 
-func _on_change_direction():
-	direction.x *= -1
-	direction.y *= -1
+func _on_change_direction(dir: Vector2, mult: float):
+	var dir_length = direction.length()
+	if (!dir_length or dir_length < SPEED):
+		dir_length = SPEED
+	var new_direction = dir.normalized() * dir_length
+	direction = new_direction * mult
--- Character.gd
+++ Character.gd
@@ -6,11 +6,13 @@ const SPEED = 600.0
 const JUMP_VELOCITY = -1500.0
 const EXTRA_JUMPS = 1
 const SWING_TIME = 0.2
+const SWING_MULT = 1.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_count = EXTRA_JUMPS
 var swinging = false
+var swing_dir: Vector2 = Vector2(0, 0)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,10 +26,9 @@ func _physics_process(delta):
 	
 	if (jump_count != EXTRA_JUMPS and is_on_floor()): jump_count = EXTRA_JUMPS
 	
-	if Input.is_action_pressed("duck") and velocity.y > 0:
-		velocity.y *= 1.1
-	
 	if Input.is_action_just_pressed("swing"):
+		var move_dir: Vector2 = Input.get_vector("move_left", "move_right", "look_up", "duck")
+		swing_dir = move_dir
 		var timer: SceneTreeTimer = get_tree().create_timer(SWING_TIME)
 		swinging = true
 		timer.timeout.connect(stop_swinging)
@@ -40,7 +41,6 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 
 func stop_swinging():
@@ -51,6 +51,9 @@ func is_ball(node: Node2D):
 func trigger_hit():
 	var bodies = %BatterBox.get_overlapping_bodies()
 	var balls: Array[Node2D] = bodies.filter(is_ball)
-	if (len(balls) > 0):
-		balls[0].emit_signal("change_direction")
+	var mult = 1
+	if (swing_dir.y > 0):
+		mult = SWING_MULT
+	for ball in balls:
+		ball.emit_signal("change_direction", swing_dir, mult)
 		swinging = false
