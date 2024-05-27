b259d69aa639df453452edb1731f4cecaad944fc
William Relken <relkenwilliam@gmail.com>
2024-05-27 19:04:19 -0400

    added leaving character select (currently only controllers can join)

--- scenes/menus/character_select/CharacterSelect.gd
+++ scenes/menus/character_select/CharacterSelect.gd
@@ -1,5 +1,6 @@
 extends Control
 
+const MAX_PLAYERS = 4
 var player_devices: Array[int] = []
 
 # Called when the node enters the scene tree for the first time.
@@ -10,11 +11,19 @@ func _ready():
 func _process(delta):
 	var players = %PlayerList.get_children()
 	for id in Input.get_connected_joypads():
-		if (MultiplayerInput.is_action_just_pressed(id, "jump") and id not in player_devices and len(players) < 4):
+		if (MultiplayerInput.is_action_just_pressed(id, "jump") and id not in player_devices and len(players) < MAX_PLAYERS):
 			var new_player = load("res://scenes/menus/character_select/player_box/PlayerBox.tscn")
 			var scene = new_player.instantiate()
 			scene.set_device_id.emit(id)
 			%PlayerList.add_child(scene)
+	player_devices = get_players()
+
+func get_players():
+	var id_list: Array[int] = []
+	var players = %PlayerList.get_children()
+	for player in players:
+		id_list.push_back(player.device_id)
+	return id_list
 
 func is_not_taken(player):
 	return !player.device_selected
--- scenes/menus/character_select/player_box/PlayerBox.gd
+++ scenes/menus/character_select/player_box/PlayerBox.gd
@@ -6,7 +6,8 @@ signal set_device_id(id: int)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (MultiplayerInput.is_action_pressed(device_id, "bunt")):
+		self.queue_free()
 
 func _on_set_device_id(id: int):
 	device_id = id
