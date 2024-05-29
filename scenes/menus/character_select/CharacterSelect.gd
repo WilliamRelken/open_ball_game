8788b6fa745ff8dec6ab5a18b34ffe28d38f36d3
WilliamRelken <will@spinspire.com>
2024-05-29 09:48:11 -0400

    fixed cyclic dep

--- scenes/menus/character_select/CharacterSelect.gd
+++ scenes/menus/character_select/CharacterSelect.gd
@@ -27,3 +27,6 @@ func get_players():
 
 func is_not_taken(player):
 	return !player.device_selected
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/main/Main.tscn")
